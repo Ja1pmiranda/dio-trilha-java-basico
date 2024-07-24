a0ce6f86c976d2225a67da5d018db61d570a6024
João Pedro Passos Miranda <ja1pmiranda@gmail.com>
2024-07-24 15:06:33 -0300

    Sistema de conta no terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        //Solicitar e ler o numero da conta
+        System.out.println("Por favor, digite o número da conta !");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); //quebra de linha
+
+        //solicitar e ler agencia
+        System.out.println("Por favor, digite o número da Agencia !");
+        String agencia = scanner.nextLine();
+        
+        //solicitar e ler nome do cliente
+        System.out.println("Por favor, digite seu nome !");
+        String nome = scanner.nextLine();
+
+        //solicitar e ler saldo
+        System.out.println("Por favor, digite seu saldo !");
+        double saldo = scanner.nextDouble();
+
+        //Exibir a menssagem
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numeroConta+
+                            " e seu saldo "+saldo+" já está disponível para saque");
+
+        
+
+        scanner.close();
     }
 }
